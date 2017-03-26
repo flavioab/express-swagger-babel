99bc0048830603499fa471c4019694356c9570ed
Flávio Briz <flavio@trocafone.com>
2017-03-26 19:16:41 -0300

    update lock and nodemon added

--- api/controllers/hello_world.js
+++ api/controllers/hello_world.js
@@ -1,16 +1,4 @@
-'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
-
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
-var util = require('util');
+import util from 'util';
 
 /*
  Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
